dddb6fd649f3f3f4d8e63a91ba7dbf7525c25e09
Akiomi Kamakura <akiomik@gmail.com>
2013-10-15 05:36:07 +0900

    added giter8 templates

---
+++ src/main/g8/build.sbt
@@ -0,0 +1,8 @@
+name := "$name$"
+
+organization := "$organization$"
+
+libraryDependencies ++= Seq(
+    "com.imageworks.scala-migrations" %% "scala-migrations" % "1.1.1"
+)
+
---
+++ src/main/g8/src/main/scala/$organization__packaged$/Migrate_$datetime$_$migration_name__Camel$.scala
@@ -0,0 +1,14 @@
+package $organization$
+
+import com.imageworks.migration._
+
+class Migrate_$datetime$_$migration_name;format="Camel"$
+  extends Migration
+{
+  def up() {
+  }
+
+  def down() {
+  }
+}
+
